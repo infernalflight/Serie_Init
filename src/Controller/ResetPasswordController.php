dd6412941a53f0a47d2813b44fc2142f3ea4f3b5
infernalflight <f.fotre@gmail.com>
2023-10-11 14:21:18 +0200

    Connexion avec username ou email

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\ChangePasswordFormType;
+use App\Form\ChangePasswordType;
 use App\Form\ResetPasswordRequestFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
@@ -102,7 +102,7 @@ class ResetPasswordController extends AbstractController
         }
 
         // The token is valid; allow the user to change their password.
-        $form = $this->createForm(ChangePasswordFormType::class);
+        $form = $this->createForm(ChangePasswordType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
